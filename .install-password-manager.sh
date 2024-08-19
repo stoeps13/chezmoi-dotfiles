9e37cac02c8d0c2c3b4c9036e309c9874db11ed0
Christoph Stoettner <christoph.stoettner@stoeps.de>
2024-08-19 22:21:23 +0200

    Initial commit

---
+++ .install-password-manager.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Linux)
+    if [[ ! -f ~/.local/bin/bw ]];
+    then
+      curl --location --output /tmp/bw-cli.zip 'https://vault.bitwarden.com/download/?app=cli&platform=linux'
+      unzip -d /tmp/bw /tmp/bw-cli.zip
+      mv /tmp/bw/bw ~/.local/bin/
+      chmod +x ~/.local/bin/bw
+      rm -rf /tmp/bw /tmp/bw-cli.zip
+    fi
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
