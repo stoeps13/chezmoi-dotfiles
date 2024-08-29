2236026dd51148761721e7d50069e8811969ac0d
Christoph Stoettner <christoph.stoettner@stoeps.de>
2024-08-29 09:39:32 +0200

    Rewrite neovim config

--- dot_config/nvim/lua/core/plugin_config/treesitter.lua
+++
@@ -1,8 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { 'lua', 'vim', 'markdown', 'yaml'},
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-}
