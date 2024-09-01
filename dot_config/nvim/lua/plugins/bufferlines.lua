efca228032dd39ffc95bf49e9e1bc9f1a3a711e2
Christoph Stoettner <christoph.stoettner@stoeps.de>
2024-09-01 21:47:50 +0200

    update bufferline and add tailspin

--- dot_config/nvim/lua/plugins/bufferlines.lua
+++ dot_config/nvim/lua/plugins/bufferlines.lua
@@ -1,22 +1,26 @@
 return {
   "akinsho/bufferline.nvim",
-  dependencies = 'nvim-tree/nvim-web-devicons',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'moll/vim-bbye'
+  },
   config = function()
     require("bufferline").setup {
       options = {
-        mode = 'tabs',
-        seperator_style = 'slant',
-        show_close_icon = false,
-        show_buffer_close_icon = false,
-        diagnostics = 'nvim_lsp',
         always_show_bufferline = true,
+        diagnostics = 'nvim_lsp',
+        enforce_regular_tabs = true,
         numbers = "none",
-        close_command = function(n)
-          LazyVim.ui.bufremove(n)
-        end,
-        right_mouse_command = function(n)
-          LazyVim.ui.bufremove(n)
-        end,
+        persist_buffer_sort = true,
+        seperator_style = 'thin',
+        show_buffer_close_icons = false,
+        show_buffer_icons = true,
+        show_close_icon = false,
+        show_tab_indicators = true,
+        indicator = {
+          style = 'icon',
+          icon = '|',
+        },
         hover = {
           enabled = true,
           delay = 150,
@@ -25,14 +29,14 @@ return {
         offsets = {
           {
             filetype = "neo-tree",
-            text = "File Explorer",
-            highlight = "Directory",
-            text_align = "left"
+            text = "",
+            padding = 1
           }
         },
       }
     }
     vim.keymap.set("n", "H", ":bp<CR>", {})
     vim.keymap.set("n", "L", ":bn<CR>", {})
+    vim.keymap.set('n', "<leader>q", ":Bdelete! %<CR>", {})
   end,
 }
