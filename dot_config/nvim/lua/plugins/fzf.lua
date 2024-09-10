ab04029dfcc4b5b3b39087922aa30f5c526ffe81
Christoph Stoettner <christoph.stoettner@stoeps.de>
2024-09-10 06:29:39 +0200

    Switch to fzf

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,15 +1,18 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "<leader>fd", "<cmd>FzfLua files<cr>",    desc = "Find files" },
-    { "<leader>rg", "<cmd>FzfLua grep<cr><cr>", desc = "Grep files" },
-    { "<leader>cc", "<cmd>FzfLua grep_quickfix<cr>", desc = "Grep files" },
-    { "<leader>b",  "<cmd>FzfLua buffers<cr>",  desc = "Find buffers" },
-  },
-  config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({ "fzf-tmux" })
-  end,
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "<leader>fd", "<cmd>FzfLua files<cr>", desc = "Find files" },
+		{ "<leader>rg", "<cmd>FzfLua live_grep<cr><cr>", desc = "Live Grep" },
+		{ "<leader>b", "<cmd>FzfLua buffers<cr>", desc = "Buffers" },
+		{ "<leader>m", "<cmd>FzfLua marks<cr>", desc = "Marks" },
+		{ "<leader>gs", "<cmd>FzfLua git_status<cr>", desc = "Git status" },
+		{ "<leader>c", "<cmd>FzfLua lsp_code_actions<cr>", desc = "Lsp Code Actions" },
+		{ "<leader>cd", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document diagnostics" },
+	},
+	config = function()
+		-- calling `setup` is optional for customization
+		require("fzf-lua").setup({ "fzf-tmux" })
+	end,
 }
