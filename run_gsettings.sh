9e7bcddf1ce90f52a6da1adab430fd7dfed64e48
Christoph Stoettner <christoph.stoettner@stoeps.de>
2024-09-23 07:23:39 +0200

    Set Gnome config

--- run_gsettings.sh
+++ run_gsettings.sh
@@ -2,3 +2,9 @@
 
 # Disable Nautilus recent files
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+# Set compose key
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none', 'eurosign:e', 'compose:rctrl']"
+
+# Set touchpad tap to click
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
