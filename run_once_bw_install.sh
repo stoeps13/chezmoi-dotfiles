5c8888bea368d12e2d0975d3703126d8e67ca8f2
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-03-02 17:46:15 +0100

    add bw install

---
+++ run_once_bw_install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Linux)
+    if [ ! -f ~/.local/bin/bw ];
+    then
+      dir=$(mktemp -d)
+      curl --location --output $dir/bw-cli.zip 'https://vault.bitwarden.com/download/?app=cli&platform=linux'
+      unzip -d $dir/bw $dir/bw-cli.zip
+      if [ ! -d ~/.local/bin ];
+      then
+        mkdir -p ~/.local/bin
+      fi
+      mv $dir/bw/bw ~/.local/bin/
+      chmod +x ~/.local/bin/bw
+    fi
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
