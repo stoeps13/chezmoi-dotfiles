717709a2aae0b818b5f7689e049bc786d071b759
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-03-02 19:01:32 +0100

    update bw install

--- run_once_bw_install.sh
+++ run_once_bw_install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# exit immediately if password-manager-binary is already in $PATH
-type password-manager-binary >/dev/null 2>&1 && exit
-
 case "$(uname -s)" in
 Linux)
     if [ ! -f ~/.local/bin/bw ];
