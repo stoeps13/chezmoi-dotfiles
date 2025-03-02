623c56c2ef4da762d537856fe88894b0d2c479c1
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-03-02 22:10:21 +0100

    .

--- run_once_nvim_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-dir=$(mktemp -d)
-cd $dir
-curl -L -O https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-tar xvzf $dir/nvim-linux-x86_64.tar.gz -C ~/.local/share/
-ln -s ~/.local/share/nvim-linux-x86_64/bin/nvim ~/.local/bin/nvim
