ef9c244a4a68b32fd6c2947a7cd3c35cb0a50612
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-05-14 11:14:05 +0200

    update script

--- run_after_nvim-symlink.sh
+++ run_after_nvim-symlink.sh
@@ -3,10 +3,11 @@
 export strFile="$HOME/.local/bin/nvim"
 if [[ -L "$strFile" ]] && [[ ! -a "$strFile" ]]
 then
-    echo 'Nvim symlink not working'
+    # broken symlink for nvim
     rm -f "$HOME/.local/bin/nvim"
     ln -s "$HOME/.local/share/nvim-linux-x86_64/bin/nvim" "$HOME/.local/bin/nvim"
 elif ! [ -e "$strFile" ]
 then
+    # symlink not existing
     ln -s "$HOME/.local/share/nvim-linux-x86_64/bin/nvim" "$HOME/.local/bin/nvim"
 fi
