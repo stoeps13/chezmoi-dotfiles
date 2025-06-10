c339035526bb8de005f77adbc8171d8d3a9ebf34
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-06-10 11:22:23 +0200

    Update .local/bin/sn_interloc_overview.py

--- dot_local/bin/executable_sn_interloc_overview.py
+++ dot_local/bin/executable_sn_interloc_overview.py
@@ -16,7 +16,7 @@ except ImportError:
 username, password, base_url = load_config()
 client = ServiceNowClient(base_url, (username, password))
 
-gr = client.GlideRecord('sn_customerservice_case')
+gr = client.GlideRecord("sn_customerservice_case")
 gr.add_query("active", "true")
 gr.order_by("task_effective_number")
 gr.query()
@@ -27,18 +27,24 @@ cases_by_priority = defaultdict(list)
 today = datetime.today()
 
 for r in gr:
-    case_no = r.get_display_value('task_effective_number')
-    case_title = r.get_display_value('short_description')
-    case_prio = r.get_display_value('priority')
-    created = datetime.strptime(r.get_display_value('sys_created_on'), '%Y-%m-%d %H:%M:%S')
+    case_no = r.get_display_value("task_effective_number")
+    case_title = r.get_display_value("short_description")
+    case_prio = r.get_display_value("priority")
+    created = datetime.strptime(
+        r.get_display_value("sys_created_on"), "%Y-%m-%d %H:%M:%S"
+    )
     age = (today - created).days
-    age_in_months = relativedelta(today, created).months + (relativedelta(today, created).years * 12)
+    age_in_months = relativedelta(today, created).months + (
+        relativedelta(today, created).years * 12
+    )
 
     # Store cases in dictionary grouped by priority
-    cases_by_priority[case_prio].append(f'- [{case_no}: {case_title}](../hcl-cases/{case_no}) - age: {age} days or {age_in_months} months')
+    cases_by_priority[case_prio].append(
+        f"- [{case_no}: {case_title}](../hcl-cases/{case_no}) - age: {age} days or {age_in_months} months"
+    )
 
 # Print markdown output grouped by priority
 for prio in sorted(cases_by_priority.keys()):  # Ensure sorted order
-    print(f"## Cases Prio {prio}\n")
+    print(f"## Cases prio {prio.lower()}\n")
     print("\n".join(cases_by_priority[prio]))
     print()  # Extra newline for spacing
