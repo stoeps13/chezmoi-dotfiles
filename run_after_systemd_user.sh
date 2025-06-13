789d78bc091f257c8f891f3b02da6afe317cdbe3
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-06-13 12:03:39 +0200

    add ssh askpass config

--- run_after_systemd_user.sh
+++ run_after_systemd_user.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in davmail.service  mbsync.timer syncthing.service vdirsync.timer
+for i in davmail.service  mbsync.timer syncthing.service vdirsync.timer ssh-agent.service
 do
         systemctl enable --user $i
 done
