32e10c7b53b1d8d82342b1c1da2d66d3237f4788
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-07-09 18:45:56 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,15 +1,15 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'FiraCode Nerd Font'
-config.font_size = 9
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 11
 -- For example, changing the color scheme:
-config.color_scheme = 'Nord (Gogh)'
-config.enable_tab_bar = false
+config.color_scheme = "Nord (Gogh)"
+config.enable_tab_bar = true
 config.enable_wayland = true
 config.window_decorations = "RESIZE"
 -- config.window_background_opacity = 0.9
