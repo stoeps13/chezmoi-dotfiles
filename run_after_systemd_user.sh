feca1c36cdb758ddd4329f6369fc47e27a6b567c
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-10-16 16:53:45 +0200

    remove davmail service, using flatpak now

--- run_after_systemd_user.sh
+++ run_after_systemd_user.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in davmail.service mbsync.service mbsync.timer syncthing.service vdirsync.service vdirsync.timer chezmoi-init.service chezmoi-update.timer
+for i in mbsync.service mbsync.timer syncthing.service vdirsync.service vdirsync.timer chezmoi-init.service chezmoi-update.timer
 do
         systemctl enable --user $i
 done
