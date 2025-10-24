786a84fedca46ee812f04aeb91d4d733874095e8
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-10-24 07:14:57 +0200

    Update .chezmoiexternal.toml Update .local/bin/sn_case_with_comments.py Update .tcc/user.conf Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,13 +6,14 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 11
+config.font_size = 10
 -- For example, changing the color scheme:
-config.color_scheme = "Nord (Gogh)"
-config.enable_tab_bar = true
-config.enable_wayland = true
-config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.9
+-- config.color_scheme = "Nord (Gogh)"
+config.color_scheme = "Tokyo Night (Gogh)"
+config.enable_tab_bar = false
+-- config.enable_wayland = true
+-- config.window_decorations = "RESIZE"
+-- config.window_background_opacity = 0.95
 
 -- and finally, return the configuration to wezterm
 return config
