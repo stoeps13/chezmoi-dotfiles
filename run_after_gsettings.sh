50e91a3899c66c5be65fa7ce1e5cbf08f640f140
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-11-28 10:14:35 +0100

    Update .mbsyncrc Update gsettings.sh

--- run_after_gsettings.sh
+++ run_after_gsettings.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-type gsettings >/dev/null 2>&1 || exit 0
+# echo "Desktop session: $DESKTOP_SESSION"
 
-# Disable Nautilus recent files
-gsettings set org.gnome.desktop.privacy remember-recent-files false
-
-# Set compose key
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none', 'eurosign:e', 'compose:rctrl', 'caps:ctrl_modifier']"
-
-# Set touchpad tap to click
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-
-# Switch workspace on all displays
-gsettings set org.gnome.mutter workspaces-only-on-primary false
+if [[ $DESKTOP_SESSION = "gnome" ]]
+  then
+    echo "Gnome session detected"
+    type gsettings >/dev/null 2>&1 || exit 0
+    
+    # Disable Nautilus recent files
+    gsettings set org.gnome.desktop.privacy remember-recent-files false
+    
+    # Set compose key
+    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none', 'eurosign:e', 'compose:rctrl', 'caps:ctrl_modifier']"
+    
+    # Set touchpad tap to click
+    gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+    
+    # Switch workspace on all displays
+    gsettings set org.gnome.mutter workspaces-only-on-primary false
+  else
+    echo "Skip gsettings script, no gnome session detected!"
+fi
