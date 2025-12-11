4484648c2e8ef6117da3e7ede32f93dc574ea15b
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-12-11 10:31:46 +0100

    Update .config/tmuxp/work.yaml Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -8,8 +8,13 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 13
 -- For example, changing the color scheme:
--- config.color_scheme = "Nord (Gogh)"
 config.color_scheme = "Tokyo Night (Gogh)"
+-- alternative dark themes
+-- config.color_scheme = "Nord (Gogh)"
+-- config.color_scheme = "Tartan (terminal.sexy)"
+-- config.color_scheme = "Tomorrow Night"
+-- Light theme
+-- config.color_scheme = "tokyonight-day"
 config.enable_tab_bar = true
 -- config.enable_wayland = true
 -- config.window_decorations = "RESIZE"
