8a0fd797c013971cfbfbf7cbac9fa9fc40a9a2e9
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-04-10 20:23:37 +0200

    Remove helm_plugins.sh

--- run_after_helm_plugins.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-helm plugin install https://github.com/databus23/helm-diff --verify=false || echo "plugin already installed"
-
-if [[ -f ~/.local/bin/helm ]]
-then
-  helm plugin update diff && echo "Update successful"
-fi
