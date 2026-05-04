e192eb0c3c3403b4b6f07568ec42dbc534b91a7b
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-05-04 09:25:56 +0200

    package install with arch tools

--- run_after_pip.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-pip3 install --user --break-system-packages pynvim six packaging tasklib
---
+++ run_onchange_python.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo pacman -Sy python-pynvim python-six python-packaging python-tasklib
