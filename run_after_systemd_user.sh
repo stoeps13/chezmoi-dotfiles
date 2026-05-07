d4e8b742559ac42f7a212ca9272c0c4e79bfc3c5
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-05-07 20:16:58 +0200

    Add .chezmoiexternals/archive-files.toml Update .chezmoiexternals/files.toml Remove .oh-my-zsh-custom/completions/_task Remove .oh-my-zsh-custom/plugins/zsh-chezmoi.zsh Remove .oh-my-zsh-custom/plugins/zsh-hugo.zsh Remove .config/containers/systemd/fedora-distrobox.container Remove .config/flameshot/flameshot.ini Remove .config/hypr/hypridle.conf Remove .config/hypr/hyprland.conf Remove .config/hypr/hyprlock.conf Remove .config/hypr/hyprpaper.conf Remove .config/liquidpromptrc Remove .config/rofi/config.rasi Remove .config/sway/config Remove .config/sway/config.d/60-bindings-screenshot.conf Remove .config/sway/config.d/70-screen-scale.conf Remove .config/tmuxp/work.yaml Remove .config/waybar/config.jsonc Remove .config/waybar/power_menu.xml Remove .config/waybar/style.css Remove .config/wezterm/wezterm.lua Update systemd_user.sh Update pacman.sh Update python.sh

--- run_after_systemd_user.sh
+++ run_after_systemd_user.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in mbsync.service mbsync.timer syncthing.service vdirsync.service vdirsync.timer chezmoi-init.service chezmoi-update.timer ssh-agent.socket
+for i in mbsync.service mbsync.timer syncthing.service vdirsync.service vdirsync.timer 
 do
         systemctl enable --user $i
 done
--- run_onchange_python.sh
+++ run_onchange_python.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy python-pynvim python-six python-packaging python-tasklib
+sudo pacman -Sy python-pynvim python-six python-packaging python-tasklib python-html2text python-xlrd python-jmespath python-uv
